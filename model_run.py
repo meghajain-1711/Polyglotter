6ee8f9974c8ed70e100b2fcb5964aeeb7b3960d8
thedatumgirl <59704142+TheDatumGirl@users.noreply.github.com>
2021-08-01 19:12:41 +0530

    Arg Parser in main run
    #12
    Added arg parser for config file and embeddings.
    I config file is passed , generate config is not needed , else generate_config is run with the remaining parameters

--- model_run.py
+++ model_run.py
@@ -1,4 +1,5 @@
 from sys import argv
+import argparse
 from torch import cuda
 from onmt.utils.logging import init_logger
 from onmt.utils.misc import set_random_seed
@@ -8,20 +9,37 @@ from NLP import RandomQueryGenerator
 from NLP import TrainModels, CalculateStatistics
 from RandomQueryGenerator import Preprocess
 
+def parse_args(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--config",
+        help="specify config_file which will be used for your model generation", action='store',dest='config_file'
+    )
+    parser.add_argument(
+        "--embeddings",
+        help="specify embedding_file which will be used for your model generation", action='store',dest='cembeddings'
+    )
+    args,remaining = parser.parse_known_args(argv)
+    return args,remaining
 
-def main(dataset = "HUMANMINE", datasetDir):    
+
+def main(dataset = "HUMANMINE", datasetDir, config_file):    
     init_logger()
     is_cuda = cuda.is_available()
     set_random_seed(1111, is_cuda)
 
+    config_file=genconfig.gen_preprocess_config(trainingDataSaveDir=trainingDataSaveDir, train_set_size=training_set_size )
+
     #Preprocess step 
     if datasetDir == None and dataset in defaults.configurations(dataset,1): 
-        training_set_sizes = [1000,5000,10000,25000,50000,100000,1000000]
+        training_set_sizes = defaults.configurations(training_set_sizes,1)
         for training_set_size in training_set_sizes:
             dataset_schema=defaults.configurations(dataset_schema(dataset),2)
             trainingDataDir=defaults.configurations(trainingdir(dataset),2)
             Preprocess.gen_dataset(dataset_schema,trainingDataDir,training_set_size)
             Preprocess.build_vocab(trainingDataDir,training_set_size)
+
+            TrainModels
     
     ##TODO : Add step for datasetDir given through arg
 
@@ -35,8 +53,9 @@ def main(dataset = "HUMANMINE", datasetDir):
 
 
 if __name__ == '__main__':  
-    print(f"Name of the script      : {argv[0]}")
-    print(f"Arguments of the script : {argv[1:]}")
-
-    params = genconfig.gen_defaults_config(argv[1:])
-    exit_code = main(**params)
+    args,gen_config_args=parse_args(argv[1:])
+    if args.config_file ==None:    
+        params = genconfig.gen_defaults_config(gen_config_args)
+    
+    ##TO DO : call the main pipeline here
+    
