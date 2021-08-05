ed3acba4cefa54f848ee21aa946a9466b3e4ae22
thedatumgirl <59704142+TheDatumGirl@users.noreply.github.com>
2021-08-05 23:33:49 +0530

    Update format checking
    Shifted back to Cerberus design to provide for flexibility later on

--- model_run.py
+++ model_run.py
@@ -9,6 +9,23 @@ from NLP import RandomQueryGenerator
 from NLP import TrainModels, CalculateStatistics
 from RandomQueryGenerator import Preprocess
 
+import yaml
+from cerberus import Validator
+
+def load_doc(conf_file):
+    with open(conf_file,'r') as stream:
+        try:
+            return yaml.load(stream)
+        except yaml.YAMLError as exception:
+            raise exception
+
+def validate_config_file(schema,conf_file):
+    schem=eval(open(schema,'r').read())
+    v=Validator(schem)
+    doc=load_doc(conf_file)
+    print(v.validate(doc,schem))
+    print(v.errors)
+
 def parse_args(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -38,18 +55,12 @@ def main(dataset = "HUMANMINE", datasetDir, config_file):
             trainingDataDir=defaults.configurations(trainingdir(dataset),2)
             Preprocess.gen_dataset(dataset_schema,trainingDataDir,training_set_size)
             Preprocess.build_vocab(trainingDataDir,training_set_size)
-
-            TrainModels
+            ##TODO : Add training step here  
     
-    ##TODO : Add step for datasetDir given through arg
-
-    ##TODO : Add training step 
-    
-    ##TODO : Add Evaluation step
+    ##TODO : Add Evaluation step on the model
     #Add Evaluation step Calculate Stats
     CalculateStatistics
     
-    return 0
 
 
 if __name__ == '__main__':  
@@ -57,5 +68,10 @@ if __name__ == '__main__':
     if args.config_file ==None:    
         params = genconfig.gen_defaults_config(gen_config_args)
     
+    #TO DO: Add format checking of the config file here
+    if args.config_file is not None:
+        v=validate_config_file(conf_schema,args.config_file)
+        if v==0:
+            sys.exit("Your config file is of wrong format, check the correct format from the README")
     ##TO DO : call the main pipeline here
     
